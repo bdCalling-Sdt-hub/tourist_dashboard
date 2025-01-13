fa879b21ea8a80f53e619190ce7376f607d29aa4
shaharulsiyam <shaharulsiyam0273@gmail.com>
2025-01-13 12:30:28 +0600

    s

--- src/Components/Events/EventAddEditForm.jsx
+++ src/Components/Events/EventAddEditForm.jsx
@@ -49,8 +49,8 @@ const EventAddEditForm = ({ selectedData, closeModal }) => {
       .toISOString();
     values.end_date = dayjs(values?.end_date).toDate().toISOString();
     values.time = dayjs(values?.time).format("hh:mm A");
-    values.latitude = locationData?.lat;
-    values.longitude = locationData?.lng;
+    values.latitude = locationData?.lng;
+    values.longitude = locationData?.lat;
     values.description = text;
     values.recurrence = renew;
     if (isFeatured) {
